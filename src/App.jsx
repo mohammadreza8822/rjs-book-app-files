0c2d2648e91e581cf0024aeb0a9f2dfa4e76e63f
Mohammadreza <ma3968312@gmail.com>
2024-11-21 18:37:20 +0330

    initial commit book store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import image from "./assets/1.png";
+
 function App() {
   return (
     <>
       <h1>Book App Starting files</h1>
+      <img src={image} alt="" />
+      {/* <img src="/1.png" alt="" /> if we put image in public just we need to add / */}
     </>
   );
 }
